a1d6d80e33e55fe50220231d194b52311d566f2d
zeus <zeus.ramirez347@gmail.com>
2021-05-17 16:19:23 -0400

    commit tree 3e05c177a95955453aa614451cec41517247fe85

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength <= 400){
+    return "This one is on me!"
+  }else if (rideLength < 2500){
+    return "I will gladly take your thirty bucks."
+  }else
+    return "No can do."
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city == "NYC" ? 'Ok, sounds good.' : 'No go.')
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
